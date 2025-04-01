30d4962e5c04eb3b157ca04724ae79a2e7797c5d
Memo Khoury <majd.khoury93@gmail.com>
2025-04-01 14:04:00 +0100

    readded material mesh

--- src/components/ItemGrid.tsx
+++ src/components/ItemGrid.tsx
@@ -30,21 +30,21 @@ const VoxelItem = ({ type, isOwned, isConnected }: { type: string; isOwned: bool
     // Determine material properties based on connection and ownership
     const materialProps = isConnected 
       ? (isOwned ? { color: "#FFFFFF" } : { color: "#888888", transparent: true, opacity: 0.6 })
-      : { color: "#AAAAAA", transparent: true, opacity: 0.8 };  // Not connected, show as semi-locked
+      : { color: "#AAAAAA", transparent: true, opacity: 0.8 };
 
     switch (type) {
       case "head":
         return (
           <mesh>
             <boxGeometry args={[0.8, 0.5, 0.8]} />
-            <meshStandardMaterial {...materialProps} />
+            <meshStandardMaterial color="#0044FF" />
           </mesh>
         );
       case "body":
         return (
           <mesh>
             <boxGeometry args={[1, 1.2, 0.4]} />
-            <meshStandardMaterial {...materialProps} />
+            <meshStandardMaterial color="#6633AA" />
           </mesh>
         );
       case "weapon":
@@ -52,11 +52,11 @@ const VoxelItem = ({ type, isOwned, isConnected }: { type: string; isOwned: bool
           <group>
             <mesh position={[0, 0.6, 0]}>
               <boxGeometry args={[0.2, 0.8, 0.2]} />
-              <meshStandardMaterial {...materialProps} />
+              <meshStandardMaterial color="#FF2222" />
             </mesh>
             <mesh position={[0, 0, 0]}>
               <boxGeometry args={[0.5, 0.2, 0.1]} />
-              <meshStandardMaterial {...materialProps} />
+              <meshStandardMaterial color="#FF2222" />
             </mesh>
           </group>
         );
@@ -64,7 +64,7 @@ const VoxelItem = ({ type, isOwned, isConnected }: { type: string; isOwned: bool
         return (
           <mesh>
             <boxGeometry args={[0.1, 0.8, 0.8]} />
-            <meshStandardMaterial {...materialProps} />
+            <meshStandardMaterial color="#FFCC00" />
           </mesh>
         );
       case "feet":
@@ -72,11 +72,11 @@ const VoxelItem = ({ type, isOwned, isConnected }: { type: string; isOwned: bool
           <group>
             <mesh position={[-0.25, 0, 0]}>
               <boxGeometry args={[0.3, 0.2, 0.5]} />
-              <meshStandardMaterial {...materialProps} />
+              <meshStandardMaterial color="#33AA55" />
             </mesh>
             <mesh position={[0.25, 0, 0]}>
               <boxGeometry args={[0.3, 0.2, 0.5]} />
-              <meshStandardMaterial {...materialProps} />
+              <meshStandardMaterial color="#33AA55" />
             </mesh>
           </group>
         );
@@ -91,7 +91,7 @@ const VoxelItem = ({ type, isOwned, isConnected }: { type: string; isOwned: bool
         return (
           <mesh>
             <boxGeometry args={[0.5, 0.5, 0.5]} />
-            <meshStandardMaterial {...materialProps} />
+            <meshStandardMaterial color="#AAAAAA" emissive="#333333" />
           </mesh>
         );
     }
